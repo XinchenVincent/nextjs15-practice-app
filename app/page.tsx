c391f013d15a4ab0785423454cfba20a5f90e86c
vincent <shixinchenhg@icloud.com>
2026-02-15 14:17:07 +0800

    Light & Dark Theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Hello World</h1>
-      <h1 className="h1-bold font-space-grotesk">Hello World</h1>
-    </div>
-  );
+  return <div></div>;
 }
