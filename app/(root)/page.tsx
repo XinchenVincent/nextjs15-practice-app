81a2dd0a5ff7f504dc1f4ec8e0b0478ab3f4024a
vincent <shixinchenhg@icloud.com>
2026-02-15 23:25:29 +0800

    github & google auth

---
+++ app/(root)/page.tsx
@@ -0,0 +1,23 @@
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+export default async function Home() {
+  const session = await auth();
+
+  console.log(session);
+  return (
+    <div>
+
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit">Log out</Button>
+      </form>
+    </div>
+  );
+}
